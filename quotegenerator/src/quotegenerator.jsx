d71cdcab9434cf16ee1698004f6fc732990785a6
Lavicka13 <luky.lavicka@gmail.com>
2024-11-18 12:59:33 +0100

    Background style

--- quotegenerator/src/quotegenerator.jsx
+++ quotegenerator/src/quotegenerator.jsx
@@ -1,4 +1,5 @@
-import { Button, Paper, Title, Text } from '@mantine/core';
+import { Button, Paper, Title, Text, Container, BackgroundImage
+ } from '@mantine/core';
 import React, { useState, useEffect } from "react";
 
 function Quotegenerator() {
@@ -23,29 +24,39 @@ function Quotegenerator() {
   const { content, author } = quotes;
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '100vh',
-        backgroundColor: '#f5f5f5',
-      }}
-    >
-        <Title>Welcome to the random quote generator</Title>
-      <Paper shadow="xl" radius="md" p="md" withBorder mt="xl">
-       
+    <Container size={420} my={40} style={{ position: 'relative' }}>
+    <BackgroundImage src="/quotegenerator/src/bg.jpg">
+    <Title align="center">Welcome to the random quote generator</Title>
+    <Paper 
+        shadow="xl" 
+        radius="md" 
+        p="md" 
+        withBorder 
+        mt="xl" 
+        style={{
+          width: '500px',   // Fixed width
+          height: '300px',  // Fixed height
+          backdropFilter: 'blur(10px)',  // Frosted glass effect
+          background: 'rgba(255, 255, 255, 0.3)',  // Semi-transparent white background
+          borderRadius: '15px',  // Rounded corners
+          padding: '20px',  // Padding inside Paper
+        }}
+      >
+      <div style={{ width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
         <Text>{content}</Text>
         <Text> {author}</Text>
         <Button
           variant="gradient"
           gradient={{ from: 'cyan', to: 'grape', deg: 90 }}
           onClick={getNewQuote}
+          
         >
-          Generate Quote
+          Get another Quote
         </Button>
+        </div>
       </Paper>
-    </div>
+      </BackgroundImage>
+     </Container>
   );
 }
 
